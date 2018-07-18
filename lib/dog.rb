d47cda382e45f734fa3055715ac19209dfb9405a
Christina Ng <christinasy317@gmail.com>
2018-07-18 04:20:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,19 @@
 class Dog
 
   def name = (dog_name)
-    @dogs_name = dog_name 
-  
-end 
+    @name = dog_name 
+  end 
+
+  def name
+    @name 
+  end 
+
+  def breed = (dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end 
+
+end
